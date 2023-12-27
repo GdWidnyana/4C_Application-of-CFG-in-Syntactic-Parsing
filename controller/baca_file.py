a5dbbd76e7a33453d792f5bc77d42a7cf03c6dfe
GdWidnyana <120539660+GdWidnyana@users.noreply.github.com>
2023-12-27 14:50:35 +0800

    baca_file.py

--- controller/baca_file.py
+++ controller/baca_file.py
@@ -1,6 +1,16 @@
+import os
+
 def baca_file(filepath):
     # persiapkan list kosong
     data = []
+    
+    # ubah path ke format yang sesuai dengan sistem operasi
+    filepath = os.path.abspath(filepath)
+    
+    # periksa apakah file ada
+    if not os.path.isfile(filepath):
+        raise FileNotFoundError(f"The file '{filepath}' does not exist.")
+    
     # buka file txt dengan mode baca
     with open(filepath, 'r') as file: 
         # baca setiap aturan baris per baris
@@ -14,9 +24,13 @@ def baca_file(filepath):
     return data
 
 # Ubah filepath sesuai dengan path yang diinginkan
-filepath = r'C:\Users\Widnyana\Downloads\4c\Program\data\4c_CNF.txt'
+filepath = 'data/4C_CNF.txt'
 
-# Panggil fungsi baca_file dengan filepath yang baru
-data_cnf = baca_file(filepath)
-
-# Lakukan operasi lain dengan data_cnf jika diperlukan
+try:
+    # Panggil fungsi baca_file dengan filepath yang baru
+    data_cnf = baca_file(filepath)
+    
+    # Lakukan operasi lain dengan data_cnf jika diperlukan
+    print(data_cnf)
+except FileNotFoundError as e:
+    print(e)
