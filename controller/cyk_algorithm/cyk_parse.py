5623e7c0e2401ca3a3e6a410c1da44505faef133
GdWidnyana <120539660+GdWidnyana@users.noreply.github.com>
2024-01-02 21:33:39 +0800

    cyk_parse.py

--- controller/cyk_algorithm/cyk_parse.py
+++ controller/cyk_algorithm/cyk_parse.py
@@ -7,7 +7,7 @@ from view.table import print_table
 
 # Fungsi untuk mengurai kalimat dengan algoritma CYK
 def parse(cnf, daftar_kata):
-    # Persiapkan tabel pengisian
+    placeholder = st.empty()
     tabel = create_table(daftar_kata)
     # Tampilkan tabel ke web
     st.write(print_table(tabel, daftar_kata), unsafe_allow_html=True)
@@ -17,5 +17,13 @@ def parse(cnf, daftar_kata):
     # Tampilkan tabel lagi ke web
     st.write(print_table(tabel, daftar_kata), unsafe_allow_html=True)
 
-    # Isi semua tabel pengisian dan tampilkan status penerimaan
-    filling_all(cnf, tabel, daftar_kata)
+    # Isi semua tabel pengisian dan dapatkan status penerimaan
+    is_accepted, tabel = filling_all(cnf, tabel, daftar_kata)
+    
+    # Logika untuk menampilkan pesan di atas tabel
+    if is_accepted:
+        placeholder.success('Kalimat diterima.')
+        st.balloons()
+    else:
+        placeholder.error('Kalimat tidak diterima karena tidak sesuai pola atau persyaratan kalimat baku bahasa Indonesia atau ada kata yang belum terdaftar di database')
+        st.snow()
